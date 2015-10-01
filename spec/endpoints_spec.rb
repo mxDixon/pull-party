54a9431724085f4e7ed8d4ff7667a69913d10437
mxDixon <christopher.dixon@careerbuilder.com>
2015-10-01 14:09:51 -0400

    failing test

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -5,7 +5,7 @@ describe App do
     describe 'POST /github_listener' do
         context 'receives a valid notification' do
             it 'creates entry for new pull request' do
-                expect(PullReqest).to receive(:new)
+                expect(PullRequest).to receive(:new)
             end
 
             it 'pushes notification to hipchat' do
