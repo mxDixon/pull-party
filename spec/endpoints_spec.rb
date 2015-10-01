f57abd69f682f71fd52a3a7dc2a722e2a8b5c9bc
mxDixon <christopher.dixon@careerbuilder.com>
2015-10-01 12:10:12 -0400

    test update

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -4,7 +4,7 @@ describe App do
     describe 'POST /github_listener' do
         context 'receives a valid notification' do
             it 'creates entry for new pull request' do
-
+                expect(PullReqest).to receive(:new)
             end
 
             it 'pushes notification to hipchat' do
